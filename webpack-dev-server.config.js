1825679bd17f080999b69c6f4c60ada7d8872e30
pp88sky <pp88sky@163.com>
2016-03-23 21:54:47 +0800

    no

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+var buildPath = path.resolve(__dirname, 'build');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+var config = {
+  //Entry points to the project
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/app.js')
+  ],
+  //Config options on how to interpret requires imports
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+    //node_modules: ["web_modules", "node_modules"]  (Default Settings)
+  },
+  //Server Configuration options
+  devServer:{
+    contentBase: 'src/www',  //Relative directory for base of server
+    devtool: 'eval',
+    hot: true,        //Live-reload
+    inline: true,
+    port: 3000,        //Port Number
+    host: '192.168.1.101'  //Change to '0.0.0.0' for external facing server
+  },
+  devtool: 'eval',
+  output: {
+    path: buildPath,    //Path of output file
+    filename: 'app.js'
+  },
+  plugins: [
+    //Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    //Allows error warnings but does not stop compiling. Will remove when eslint is added
+    new webpack.NoErrorsPlugin(),
+    //Moves files
+    new TransferWebpackPlugin([
+      {from: 'www'}
+    ], path.resolve(__dirname, "src"))
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/, 
+        loaders: ['babel-loader'], 
+        exclude: [nodeModulesPath]
+      },
+	  {test: /\.css$/, loader: "style-loader!css-loader" },
+	  {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
+
+module.exports = config;
